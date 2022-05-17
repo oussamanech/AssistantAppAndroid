30cd51eba55ed8060bf1d72e949d5e1b87c40d7f
oussamanech <oussamanach75@gmail.com>
2022-05-17 23:41:19 +0300

    add cameraManager

--- app/src/main/java/com/example/assistantmajda/assistant/AssistantActivity.kt
+++ app/src/main/java/com/example/assistantmajda/assistant/AssistantActivity.kt
@@ -24,24 +24,24 @@ import com.example.assistantmajda.databinding.ActivityAssistantBinding
 
 class AssistantActivity : AppCompatActivity() {
 
-    private  lateinit var binding:ActivityAssistantBinding
-    private  lateinit var assistantViewModel: AssistantViewModel
+    private lateinit var binding: ActivityAssistantBinding
+    private lateinit var assistantViewModel: AssistantViewModel
 
-    private  lateinit var textToSpeach: TextToSpeech
-    private  lateinit var speechRecongnize: SpeechRecognizer
-    private  lateinit var recognizeIntent: Intent
-    private  lateinit var keeper: String
+    private lateinit var textToSpeach: TextToSpeech
+    private lateinit var speechRecongnize: SpeechRecognizer
+    private lateinit var recognizeIntent: Intent
+    private lateinit var keeper: String
 
-    private  var REQUESTCALL = 1
-    private  var SENDSMS = 2
-    private  var READSMS = 3
-    private  var SHAREFILE = 4
-    private  var SHAREATEXTFILE = 5
-    private  var READCONTACTS = 6
-    private  var CAPTUREPHOTO = 7
+    private var REQUESTCALL = 1
+    private var SENDSMS = 2
+    private var READSMS = 3
+    private var SHAREFILE = 4
+    private var SHAREATEXTFILE = 5
+    private var READCONTACTS = 6
+    private var CAPTUREPHOTO = 7
 
-    private  var REQUEST_CODE_SELECT_DOC: Int = 100
-    private  var REQUEST_ENABLE_BT = 1000
+    private var REQUEST_CODE_SELECT_DOC: Int = 100
+    private var REQUEST_ENABLE_BT = 1000
 
     private var bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
     private lateinit var cameraManager: CameraManager
@@ -54,43 +54,46 @@ class AssistantActivity : AppCompatActivity() {
 //    private lateinit var helper: OpenWeatherMapHelper
 
     @Suppress("DEPRECATION")
-    private val imageDirectory = Environment.getExternalStorageState(Environment.DIRECTORY_PICTURES).toString()
+    private val imageDirectory =
+        Environment.getExternalStorageState(Environment.DIRECTORY_PICTURES).toString()
 
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_assistant)
-        overridePendingTransition(R.anim.non_movable,R.anim.non_movable)
+        overridePendingTransition(R.anim.non_movable, R.anim.non_movable)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_assistant)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_assistant)
 
         val application = requireNotNull(this).application
         val dataSource = AssistantDatabase.getInstance(application).assisstantDao
-        val viewModelFactory = AssistantViewModelFactory(dataSource,application)
+        val viewModelFactory = AssistantViewModelFactory(dataSource, application)
 
-        assistantViewModel = ViewModelProvider(this,viewModelFactory).get(AssistantViewModel::class.java)
+        assistantViewModel =
+            ViewModelProvider(this, viewModelFactory).get(AssistantViewModel::class.java)
 
         val adapter = AssistantAdapter()
         binding.recyclerview.adapter = adapter
 
-        assistantViewModel.messages.observe(this,{
-            it?.let{
+        assistantViewModel.messages.observe(this, {
+            it?.let {
                 adapter.data = it
             }
         })
         binding.setLifecycleOwner(this)
         //animation
 
-        if(savedInstanceState == null){
+        if (savedInstanceState == null) {
             binding.assistantConstraintLayout.setVisibility(View.INVISIBLE)
-            val viewTreeObserver: ViewTreeObserver = binding.assistantConstraintLayout.getViewTreeObserver()
+            val viewTreeObserver: ViewTreeObserver =
+                binding.assistantConstraintLayout.getViewTreeObserver()
 
-            if(viewTreeObserver.isAlive)
-            {
-                viewTreeObserver.addOnGlobalLayoutListener( object : ViewTreeObserver.OnGlobalLayoutListener{
+            if (viewTreeObserver.isAlive) {
+                viewTreeObserver.addOnGlobalLayoutListener(object :
+                    ViewTreeObserver.OnGlobalLayoutListener {
                     override fun onGlobalLayout() {
-                       circularReaalActivity()
+                        circularReaalActivity()
                         binding.assistantConstraintLayout.getViewTreeObserver()
                             .removeOnGlobalLayoutListener(this)
                     }
@@ -99,10 +102,17 @@ class AssistantActivity : AppCompatActivity() {
             }
 
         }
+        cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
+        try {
+            cameraID = cameraManager.cameraIdList[0]
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+
 
 
     }
-
 
 
 }
