b05be0557095d92658a59ecc69eb8f6cb8bb71ec
oussamanech <oussamanach75@gmail.com>
2022-05-17 21:08:16 +0300

    update Assistant Adapter

--- app/src/main/java/com/example/assistantmajda/assistant/AssistantAdapter.kt
+++ app/src/main/java/com/example/assistantmajda/assistant/AssistantAdapter.kt
@@ -1,4 +1,46 @@
 package com.example.assistantmajda.assistant
 
-class AssistantAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.assistantmajda.R
+import com.example.assistantmajda.data.Assistant
+
+class AssistantAdapter : RecyclerView.Adapter<AssistantAdapter.ViewHolder>() {
+
+
+    var data = listOf<Assistant>()
+
+    set(value){
+        field = value
+        notifyDataSetChanged()
+    }
+
+
+    class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val assisstantMessage: TextView = itemView.findViewById(R.id.assistant_message)
+        val humanMessage: TextView = itemView.findViewById(R.id.human_message)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val layoutInflater = LayoutInflater.from((parent.context))
+        val view = layoutInflater
+            .inflate(R.layout.assistent_item_layout,parent,false) as ConstraintLayout
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = data[position]
+        holder.assisstantMessage.text = item.assistant_message
+        holder.humanMessage.text = item.human_message
+    }
+
+    override fun getItemCount() = data.size
 }
--- app/src/main/java/com/example/assistantmajda/data/Assistant.kt
+++ app/src/main/java/com/example/assistantmajda/data/Assistant.kt
@@ -1,5 +1,23 @@
 package com.example.assistantmajda.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName="assistant_messages_table")
+
 class Assistant {
-    
+
+
+
+    @PrimaryKey(autoGenerate = true)
+    var assistantId: Long = 0L
+
+            @ColumnInfo(name = "assistant_message")
+            var assistant_message: String ="DEFAULT_MESSAGE";
+
+                    @ColumnInfo(name = "human_message")
+                    var human_message:String ="DEFAULT_MESSAGE";
+
 }
