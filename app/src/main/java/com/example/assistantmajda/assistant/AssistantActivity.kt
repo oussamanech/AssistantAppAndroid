e9e4bc9926e784b366432fdb701d43b5a743515b
oussamanech <oussamanach75@gmail.com>
2022-05-18 00:41:19 +0300

    update Asisstant Activity

--- app/src/main/java/com/example/assistantmajda/assistant/AssistantActivity.kt
+++ app/src/main/java/com/example/assistantmajda/assistant/AssistantActivity.kt
@@ -1,26 +1,31 @@
 package com.example.assistantmajda.assistant
 
 import android.annotation.SuppressLint
-import android.bluetooth.BluetoothA2dp
 import android.bluetooth.BluetoothAdapter
 import android.content.ClipboardManager
 import android.content.Intent
 import android.hardware.camera2.CameraManager
 import android.media.Ringtone
+import android.media.RingtoneManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewTreeObserver
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.assistantmajda.R
-import com.example.assistantmajda.data.Assistant
 import com.example.assistantmajda.data.AssistantDatabase
 import com.example.assistantmajda.databinding.ActivityAssistantBinding
+import java.util.*
+
 
 class AssistantActivity : AppCompatActivity() {
 
@@ -48,6 +53,12 @@ class AssistantActivity : AppCompatActivity() {
     private lateinit var clipboardManager: ClipboardManager
     private lateinit var cameraID: String
     private lateinit var ringtone: Ringtone
+
+
+    private val logtts = "TTS"
+    private val logsr = "SR"
+    private val logkeeper = "Keeper"
+
 
     private var imageIndex: Int = 0
     private lateinit var imgUri: Uri
@@ -109,8 +120,133 @@ class AssistantActivity : AppCompatActivity() {
             e.printStackTrace()
         }
 
+        clipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+
+        ringtone = RingtoneManager.getRingtone(
+            applicationContext,
+            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+        )
+
+//        helper =  OpenWeatherMapHelper(getString(R.string.OPEN_WEATHER_MAP_API_KEY));
+
+        textToSpeach = TextToSpeech(this) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                val result: Int = textToSpeach.setLanguage(Locale.ENGLISH)
+
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Log.e(logtts, "languge  Not supported")
+                } else {
+                    Log.e(logtts, "languge  supported")
+                }
+
+            } else {
+                Log.e(logtts, "initialization failed  ")
+            }
+
+        }
+
+        speechRecongnize = SpeechRecognizer.createSpeechRecognizer(this)
+        recognizeIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        recognizeIntent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        recognizeIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+
+        speechRecongnize.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(params: Bundle?) {
+                Log.d("SR", "started")
+            }
+
+            override fun onBeginningOfSpeech() {
+            }
+
+            override fun onRmsChanged(rmsdB: Float) {
+            }
+
+            override fun onBufferReceived(buffer: ByteArray?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onEndOfSpeech() {
+                Log.d("SR", "onEndOfSpeech")
+            }
+
+            override fun onError(error: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResults(bundle: Bundle?) {
+
+                val data = bundle?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+
+                if (data != null) {
+
+                    keeper = data[0]
+                    Log.d(logkeeper, keeper)
+                    when {
+                        keeper.contains("thanks") -> speak("It's my job, let me know if there is somthing")
+                        keeper.contains("welcome") -> speak("for what?")
+                        keeper.contains("clear") -> assistantViewModel.onClear()
+                        keeper.contains("date") -> getDate()
+                        keeper.contains("time") -> getTime()
+                        keeper.contains("phone call") -> makeAPhoneCall()
+                        keeper.contains("send SMS") -> sendSMS()
+                        keeper.contains("read my last SMS") -> readSMS()
+
+                        keeper.contains("open Gmail") -> openGmail()
+                        keeper.contains("phone call") -> openWhatsapp()
 
 
+                    }
+                    //end when
+
+
+                }
+            }//end en result
+
+            override fun onPartialResults(partialResults: Bundle?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onEvent(eventType: Int, params: Bundle?) {
+                TODO("Not yet implemented")
+            }
+
+        })//end speechRecongnize.setRecognitionListener
+
+        binding.assistantActionButton.setOnTouchListener { view, motionEvent ->
+
+            when (motionEvent.action) {
+                MotionEvent.ACTION_UP -> {
+                    speechRecongnize.stopListening()
+                }
+
+
+                MotionEvent.ACTION_DOWN -> {
+                    textToSpeach.stop()
+                    speechRecongnize.startListening(recognizeIntent)
+                }
+
+            }
+            false
+
+        }//end binding.assistantActionButton
+
+        checkIfSpeechRecognizerAviable()
+
+
+    }//end on create
+
+    private fun checkIfSpeechRecognizerAviable() {
+        if(SpeechRecognizer.isRecognitionAvailable(this)){
+            Log.d(logsr,"yes")
+        }else{
+            Log.d(logsr,"false")
+        }
+    }
+
+    fun speak(){
 
     }
 
