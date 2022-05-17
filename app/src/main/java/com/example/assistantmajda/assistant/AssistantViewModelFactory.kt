ee0b22f769e34b1d17961d1c35bfd229bc2ea75e
oussamanech <oussamanach75@gmail.com>
2022-05-17 21:18:47 +0300

    update AssistantViewModelFactory

--- app/src/main/java/com/example/assistantmajda/assistant/AssistantViewModelFactory.kt
+++ app/src/main/java/com/example/assistantmajda/assistant/AssistantViewModelFactory.kt
@@ -1,4 +1,23 @@
 package com.example.assistantmajda.assistant
 
-class AssistantViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.assistantmajda.data.AssistantDao
+import java.lang.IllegalArgumentException
+
+class AssistantViewModelFactory (
+    private val dataSource: AssistantDao,
+            private val application: Application) :ViewModelProvider.Factory
+{
+
+    @Suppress("UNCNECKED_CAST")
+
+    override  fun <T : ViewModel?> create(modelClass: Class<T>): T{
+        if(modelClass.isAssignableFrom(AssistantViewModel::class.java)){
+            return  AssistantViewModel(dataSource,application) as T
+        }
+        throw IllegalArgumentException("UNKOWN View Model Class")
+    }
+
 }
--- app/src/main/java/com/example/assistantmajda/data/Assistant.kt
+++ app/src/main/java/com/example/assistantmajda/data/Assistant.kt
@@ -7,17 +7,17 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName="assistant_messages_table")
 
-class Assistant {
+class Assistant (
 
 
 
     @PrimaryKey(autoGenerate = true)
-    var assistantId: Long = 0L
+    var assistantId: Long = 0L,
 
             @ColumnInfo(name = "assistant_message")
-            var assistant_message: String ="DEFAULT_MESSAGE";
+            var assistant_message: String ="DEFAULT_MESSAGE",
 
                     @ColumnInfo(name = "human_message")
-                    var human_message:String ="DEFAULT_MESSAGE";
+                    var human_message:String ="DEFAULT_MESSAGE")
 
-}
+
